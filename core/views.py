481a24c9aa85d0045dab2ab39a823029bfbe4958
larrygf02 <raulgf_02@hotmail.com>
2019-02-07 23:56:30 -0500

    Plantillas y tag url

--- core/views.py
+++ core/views.py
@@ -2,25 +2,15 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-html_base = """
-    <h1>Mi web Personal</h1>
-    <ul>
-        <li><a href="/">Portada</a></li>
-        <li><a href="/about/">About</a></li>
-        <li><a href="/portafolio/">Portafolio</a></li>
-        <li><a href="/contact/">Contacto</a></li>
-    </ul>
-"""
-
 def home(request):
     return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + "<h2>Acerca de</h2>")
+    return render(request, "core/about.html")
 
 
 def portafolio(request):
-    return HttpResponse(html_base + "<h2>Portafolio</h2>")
+    return render(request, "core/portafolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + "<h2>Contacto</h2>")
+    return render(request, "core/contact.html")
