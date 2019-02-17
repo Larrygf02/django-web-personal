edaea98c69e7d70df65c70a66399d8d1e0c66d6d
larrygf02 <raulgf_02@hotmail.com>
2019-02-17 10:24:58 -0500

    project dinamicos en la vista

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
+from core import views as core_views
+from portafolio import views as portafolio_views
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('portafolio/', views.portafolio, name="portafolio"),
-    path('contact/', views.contact, name="contact"),
+    path('', core_views.home, name="home"),
+    path('about/', core_views.about, name="about"),
+    path('portafolio/', portafolio_views.portafolio, name="portafolio"),
+    path('contact/', core_views .contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
